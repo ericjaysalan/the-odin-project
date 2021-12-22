8bd6d05552949c296d307914bcec741397426736
Eric Jay Salan <ericjaysalan@yahoo.com>
2021-12-22 19:49:39 +0800

    Finished 05_sumAll

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+  const ERROR = "ERROR";
 
+  if (firstNum < 0 || lastNum < 0) {
+    return ERROR;
+  } else if (typeof firstNum != "number" || typeof lastNum != "number") {
+    return ERROR;
+  }
+
+  if (firstNum > lastNum) {
+    let temp = firstNum;
+    firstNum = lastNum;
+    lastNum = temp;
+  }
+
+  let sum = 0;
+
+  for (let currentNum = firstNum; currentNum <= lastNum; currentNum++ ) {
+    sum += currentNum;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
