129e0c4600f5121fc5a2d4fc5b77f3ffc2fde6cb
Eric Jay Salan <ericjaysalan@yahoo.com>
2021-12-22 20:48:03 +0800

    Finished 07_tempConversion

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  const celsius = (fahrenheit  - 32) / 1.8;
+  const rounded = Math.round(celsius * 10) / 10;
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  const fahrenheit = (celsius * 1.8) + 32;
+  const rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
