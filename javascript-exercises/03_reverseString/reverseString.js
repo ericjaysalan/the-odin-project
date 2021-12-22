1df09e5abddbd44cb1aa67c609db1e453eeadba7
Eric Jay Salan <ericjaysalan@yahoo.com>
2021-12-22 16:09:25 +0800

    Finished 03_reverseString

--- javascript-exercises/03_reverseString/reverseString.js
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
 const reverseString = function(str) {
+  const stringArray = str.split("");
+  const arrayLength = stringArray.length;
+  const reverseArray = [];
+  let lastIndex = arrayLength - 1;
+  let reverseString = "";
 
+  for (let index = 0; index < stringArray.length; index++) {
+    reverseArray.push(stringArray[lastIndex]);
+    lastIndex--;
+  }
+
+  reverseArray.forEach(char => reverseString += char);
+  return reverseString;
 };
 
 // Do not edit below this line
