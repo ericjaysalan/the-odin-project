6acc43f84db7b04407d403ca116f4616e94cf2b8
Eric Jay Salan <ericjaysalan@yahoo.com>
2021-12-22 15:43:08 +0800

    Finished 01_helloWorld and 02_repeatString

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
 
+  let resultString = "";
+  for (let counter = 0; counter < num; counter++) {
+    resultString += string;
+  }
+
+  return resultString;
 };
 
 // Do not edit below this line
