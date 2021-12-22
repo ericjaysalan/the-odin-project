38eeca1df0e51d4796bab6eeb6c12c89f23d6b7e
Eric Jay Salan <ericjaysalan@yahoo.com>
2021-12-22 19:28:41 +0800

    Finished 04_removeFromArray

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toBeRemoved) {
+  for (let index = 0; index < arr.length; index++) {
+    let currentElement = arr[index];
 
+    if (checkMatch(currentElement, toBeRemoved)) {
+      arr.splice(index, 1);
+      index--;
+    }
+  }
+
+  return arr;
 };
+
+function checkMatch(currentElement, arrayToCheckFrom) {
+  let result;
+  arrayToCheckFrom.forEach(toBeRemovedElement => {
+    if (currentElement === toBeRemovedElement) {
+      result = true;
+    }
+  });
+
+  return result;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
