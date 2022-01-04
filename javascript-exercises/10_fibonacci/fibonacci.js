daa5553b6e18036f5d6f8071b99cd34cd105a2cf
Eric Jay Salan <ericjaysalan@yahoo.com>
2022-01-04 17:33:43 +0800

    Finished 10_fibonacci

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = Number(num);
+  if (num <= 0) return 'OOPS';
 
+  const fibonacciArr = createFibonacci(100);
+
+  return fibonacciArr[num - 1];
 };
 
+function createFibonacci(num) {
+  const fibonacciArr = [1];
+  let firstValue = 0;
+
+  for (let i = 0; i < num; i++) {
+    if (i === 0) firstValue = 0;
+    else firstValue = fibonacciArr[fibonacciArr.length - 2];
+
+    let secondValue = fibonacciArr[fibonacciArr.length - 1];
+    let newValue = firstValue + secondValue;
+    fibonacciArr.push(newValue);
+  }
+
+  return fibonacciArr;
+}
 // Do not edit below this line
 module.exports = fibonacci;
