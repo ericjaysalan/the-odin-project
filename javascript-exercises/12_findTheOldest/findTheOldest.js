333e62a686417493a099b0d3834d8e5cb4a44f0e
Eric Jay Salan <ericjaysalan@yahoo.com>
2022-01-04 18:23:24 +0800

    Finished 12_findTheOldest

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (arrOfObj) {
+  let oldestObj = {};
+  let oldest = 0;
 
+  arrOfObj.forEach((obj) => {
+    let death = (obj) => {
+      if ('yearOfDeath' in obj) return obj.yearOfDeath;
+      else return new Date().getFullYear();
+    };
+
+    let birthYear = obj.yearOfBirth;
+    let deathYear = death(obj);
+
+    if (deathYear - birthYear > oldest) {
+      oldestObj = obj;
+      oldest = deathYear - birthYear;
+    }
+  });
+
+  return oldestObj;
 };
 
 // Do not edit below this line
