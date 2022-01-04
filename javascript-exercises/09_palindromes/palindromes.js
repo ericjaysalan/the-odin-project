3c24cc101732777aa53db38224c9eb2d32fe63a4
Eric Jay Salan <ericjaysalan@yahoo.com>
2022-01-04 17:16:43 +0800

    Finished 09_palindromes

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let lettersOnlyStr = str.replace(/\W/g, '').toLowerCase();
+  const reverseString = str => str.split('').reverse().join('');
 
+  return lettersOnlyStr === reverseString(lettersOnlyStr) ? true : false;
 };
 
 // Do not edit below this line
